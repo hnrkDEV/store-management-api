f66db046736746059cec8a9c4a5eeaaa37175bfd
João Henrique <jhenrique.caval@hotmail.com>
2025-04-30 23:00:58 -0300

    feat: implementação do CRUD de clientes com NestJS e MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'store_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
+    ClienteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
